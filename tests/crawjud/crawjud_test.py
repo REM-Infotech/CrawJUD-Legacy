e8f3913403366a8a5eb536789f0a34b8cdb8df90
Robotz213 <nicholas@robotz.dev>
2024-12-09 17:55:01 -0400

    test socketio

--- tests/crawjud/crawjud_test.py
+++ tests/crawjud/crawjud_test.py
@@ -11,6 +11,7 @@ from flask.testing import FlaskClient
 from flask_sqlalchemy import SQLAlchemy
 from pytz import timezone
 
+from bot import WorkerThread
 from status import SetStatus as st_stt
 
 
@@ -146,16 +147,44 @@ class TestCrawJUD:
         db: SQLAlchemy = app.extensions["sqlalchemy"]
         with app.app_context():
 
-            start = SetStatus.start_bot(app, db, user, pid, 1)
-            check_return = type(start) is tuple
-            assert check_return
+            path_args, display_name = SetStatus.start_bot(app, db, user, pid, 1)
+
+            worker_thread = WorkerThread(
+                path_args=path_args,
+                display_name=display_name,
+                system="projudi",
+                typebot="capa",
+            )
+            is_started = worker_thread.start(app, db)
+
+            check_result = any([is_started == 500, is_started == 200])
+
+            assert check_result
 
     def test_status_stop(self, app: Flask, SetStatus: st_stt, create_dummy_pid):
 
         user, pid = create_dummy_pid
         db: SQLAlchemy = app.extensions["sqlalchemy"]
+
+        user = str(user)
+        pid = str(pid)
+
         with app.app_context():
 
             stop = SetStatus.botstop(db, app, pid)
-            check_return = stop is not None
-            assert check_return
+            check_return = WorkerThread().stop(12345, pid, app)
+            assert check_return is not None and stop is not None
+
+    def test_status_stop_esaj(self, app: Flask, SetStatus: st_stt, create_dummy_pid):
+
+        user, pid = create_dummy_pid
+        db: SQLAlchemy = app.extensions["sqlalchemy"]
+
+        user = str(user)
+        pid = str(pid)
+
+        with app.app_context():
+
+            stop = SetStatus.botstop(db, app, pid, "esaj", "protocolo")
+            check_return = WorkerThread().stop(12345, pid, app)
+            assert check_return is not None and stop is not None
