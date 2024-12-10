6e80eb0dc4326c7f5d53aa436ed564301fc3de52
Robotz213 <nicholas@robotz.dev>
2024-12-10 12:56:07 -0400

    moved test from crawjud to flask

--- tests/flask/response_test.py
+++ tests/flask/response_test.py
@@ -1,11 +1,19 @@
-def test_client_flask(client):
-    response = client.get("/")
-    responses = any([response.status_code == 302, response.status_code == 404])
-    assert responses
+from typing import Any
 
 
-def test_error_post_bot(client):
-    response = client.post("/bot/1/projudi/capa")
+def test_client_flask(client, app, args_bot: dict[str, str | Any], create_dummy_pid):
+    response_index = client.get("/")
+    response_bot_error = client.post("/bot/1/projudi/capa")
 
-    responses = any([response.status_code == 500, response.status_code == 404])
+    app.debug = True
+    response_bot_sucess = client.post("/bot/1/projudi/capa", **{"data": args_bot})
+
+    chk_index = response_index.status_code == 302
+
+    chk_bot_error = response_bot_error.status_code == 500
+
+    chk_bot_success = response_bot_sucess.status_code == 200
+
+    responses = all([chk_index, chk_bot_error, chk_bot_success])
+
     assert responses
