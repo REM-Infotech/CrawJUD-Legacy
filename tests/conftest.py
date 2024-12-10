55a80358251d5b1eb1a1eee7ca2e107a72084eee
Robotz213 <nicholas@robotz.dev>
2024-12-10 09:57:15 -0400

    setup test socketio #2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
+from datetime import datetime
+from os import path
+from pathlib import Path
+from typing import Any
+from uuid import uuid4
+
+import pandas as pd
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient
 from flask_socketio import SocketIO, SocketIOTestClient
 from flask_sqlalchemy import SQLAlchemy
+from pytz import timezone
 from werkzeug.datastructures import FileStorage
 
 from app import create_test_app as factory
@@ -131,3 +139,114 @@ def io(app: Flask, client: FlaskClient):
     socketio_client.connect("/log")
     yield socketio_client
     socketio_client.disconnect()
+
+
+@pytest.fixture(scope="function")
+def create_dummy_pid(app: Flask, args_bot: dict[str, str | Any]):
+
+    import random
+    import string
+
+    from app.models import BotsCrawJUD, Executions, LicensesUsers, ThreadBots
+
+    digits = random.sample(string.digits, 6)
+    process_id = int("".join(digits))
+
+    data: dict[str, str | dict[str, str]] | None = args_bot.get("data")
+
+    user = data.get("username", None)
+    pid = data.get("pid", None)
+
+    db: SQLAlchemy = app.extensions["sqlalchemy"]
+
+    with app.app_context():
+
+        db.drop_all()
+        db.create_all()
+
+        def _user_license():
+
+            from app.models import LicensesUsers, Users
+
+            license_dummy = str(uuid4())
+
+            usr = Users(login=user, nome_usuario=user, email="example@example.com")
+            usr.senhacrip = "test"
+
+            license_user = LicensesUsers(
+                name_client="dummy",
+                cpf_cnpj="12345678987654",
+                license_token=license_dummy,
+            )
+
+            usr.licenseusr = license_user
+
+            return usr, license_user
+
+        def _bots(license_user: LicensesUsers):
+
+            df = pd.read_excel(
+                path.join(Path(__file__).cwd(), "configs", "export.xlsx")
+            )
+            df.columns = df.columns.str.lower()
+            bot_info = None
+            data = []
+            for values in list(df.to_dict(orient="records")):
+
+                key = list(values)[1]
+                value = values.get(key)
+
+                chk_bot = BotsCrawJUD.query.filter_by(**{key: value}).first()
+
+                if not chk_bot:
+                    appends = BotsCrawJUD()
+
+                    for key, var in values.items():
+                        appends.__dict__.update({key: var})
+
+                    license_user.bots.append(appends)
+
+                    if (
+                        appends.system.lower() == "projudi"
+                        and appends.type.lower() == "capa"
+                    ):
+                        bot_info = appends
+
+                    data.append(appends)
+
+            db.session.add_all(data)
+
+            return license_user, bot_info
+
+        def _execut(usr, license_, bot_info):
+
+            rows = process_id
+            bt = bot_info
+            process = ThreadBots(pid=pid, processID=process_id)
+            execut = Executions(
+                pid=pid,
+                status="Em Execução",
+                arquivo_xlsx="xls_.xlsx",
+                url_socket=data.get("url_socket"),
+                total_rows=rows,
+                data_execucao=datetime.now(timezone("America/Manaus")),
+                file_output="Arguardando Arquivo",
+            )
+
+            execut.user = usr
+            execut.bot = bt
+            execut.license_usr = license_
+
+            return execut, process
+
+        usr, license_user = _user_license()
+        license_user, bot_info = _bots(license_user)
+        process, execut = _execut(usr, license_user, bot_info)
+
+        db.session.add_all([process, execut, usr, license_user])
+        db.session.commit()
+
+    yield user, pid
+
+    with app.app_context():
+        db.drop_all()
--- tests/socketio/test_socketio.py
+++ tests/socketio/test_socketio.py
@@ -1,9 +1,104 @@
 from flask_socketio import SocketIOTestClient
 
 
-def test_emit_message(io: SocketIOTestClient):
+def test_connect(io: SocketIOTestClient):
+
+    io.emit("connect", namespace="/log")
+    received = io.get_received(namespace="/log")
+
+    message_received = received[-1].get("args") == "connected!"
+
+    assert message_received
+
+
+def test_join(io: SocketIOTestClient, create_dummy_pid):
+
+    _, pid = create_dummy_pid
+
+    io.emit("join", {"pid": pid}, namespace="/log")
+    received = io.get_received(namespace="/log")
+
+    message_received = received[-1].get("args") == f"Joinned room! Room: {pid}"
+    assert message_received
+
+
+def test_disconnect(io: SocketIOTestClient):
+
+    io.emit("disconnect", namespace="/log")
+    received = io.get_received(namespace="/log")
+
+    message_received = received[-1].get("args") == "disconnected!"
+
+    assert message_received
+
+
+def test_leave(io: SocketIOTestClient, create_dummy_pid):
+
+    _, pid = create_dummy_pid
+
+    io.emit("leave", {"pid": pid}, namespace="/log")
+    received = io.get_received(namespace="/log")
+
+    message_received = received[-1].get("args") == f"Leaving Room '{pid}'"
+    assert message_received
+
+
+def test_statusbot(io: SocketIOTestClient):
 
     io.emit("statusbot", {}, namespace="/log")
     received = io.get_received(namespace="/log")
 
     assert received is not None
+
+
+def test_stop_bot(io: SocketIOTestClient, create_dummy_pid):
+
+    _, pid = create_dummy_pid
+    pid = str(pid)
+
+    io.emit("stop_bot", {"pid": pid}, namespace="/log")
+    received = io.get_received(namespace="/log")
+
+    message_received = received[-1].get("args") == "Bot stopped!"
+
+    assert message_received
+
+
+def test_terminate_bot(io: SocketIOTestClient, create_dummy_pid):
+
+    _, pid = create_dummy_pid
+    pid = str(pid)
+
+    # With PID
+    io.emit("terminate_bot", {"pid": pid}, namespace="/log")
+    received = io.get_received(namespace="/log")
+
+    message_received = received[-1].get("args") == "Bot stopped!"
+
+    # Without PID
+    io.emit("terminate_bot", {}, namespace="/log")
+    received = io.get_received(namespace="/log")
+
+    message_received_ = received[-1].get("args") == "Failed to stop bot!"
+
+    assert message_received and message_received_
+
+
+def test_log_message(io: SocketIOTestClient, create_dummy_pid):
+
+    _, pid = create_dummy_pid
+    pid = str(pid)
+
+    # With PID
+    io.emit("log_message", {"pid": pid}, namespace="/log")
+    received = io.get_received(namespace="/log")
+
+    message_received = received[-1].get("args") == "message received!"
+
+    # Without PID
+    io.emit("log_message", {}, namespace="/log")
+    received = io.get_received(namespace="/log")
+
+    message_received_ = received[-1].get("args") == "failed to receive message"
+
+    assert message_received and message_received_
