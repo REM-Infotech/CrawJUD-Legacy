9a34abe2e9c54e69bb9d85d5fa200462b099bb1e
Robotz213 <nicholas@robotz.dev>
2025-01-26 00:02:21 +0000

    Refactor: replace instance variable accesses with class variable accesses in PropertiesCrawJUD for improved consistency

--- bot/shared/__init__.py
+++ bot/shared/__init__.py
@@ -59,7 +59,7 @@ class PropertiesCrawJUD:
 
     @property
     def path(self) -> Path:
-        return self.path_
+        return PropertiesCrawJUD.path_
 
     @path.setter
     def path(self, new_path: Path) -> None:
@@ -67,11 +67,11 @@ class PropertiesCrawJUD:
         if not isinstance(new_path, Path):
             raise ValueError("Path must be a Path object")
 
-        self.path_ = new_path
+        PropertiesCrawJUD.path_ = new_path
 
     @property
     def path_args(self) -> Path:
-        return self.path_args_
+        return PropertiesCrawJUD.path_args_
 
     @path_args.setter
     def path_args(self, new_path: Path) -> None:
@@ -79,83 +79,83 @@ class PropertiesCrawJUD:
         if not isinstance(new_path, Path):
             raise ValueError("Path must be a Path object")
 
-        self.path_args_ = new_path
+        PropertiesCrawJUD.path_args_ = new_path
 
     @property
     def appends(self) -> List[str]:
-        return self.appends_
+        return PropertiesCrawJUD.appends_
 
     @appends.setter
     def appends(self, new_appends: list) -> None:
-        self.appends_ = new_appends
+        PropertiesCrawJUD.appends_ = new_appends
 
     @property
     def another_append(self) -> List[str]:
-        return self.another_append_
+        return PropertiesCrawJUD.another_append_
 
     @another_append.setter
     def another_append(self, new_another_append: list) -> None:
-        self.another_append_ = new_another_append
+        PropertiesCrawJUD.another_append_ = new_another_append
 
     @property
     def system(self) -> str:
-        return self.systembot_
+        return PropertiesCrawJUD.systembot_
 
     @system.setter
     def system(self, systembot_) -> None:
-        self.systembot_ = systembot_
+        PropertiesCrawJUD.systembot_ = systembot_
 
     @property
     def state_or_client(self) -> str:
-        return self.state_or_client_
+        return PropertiesCrawJUD.state_or_client_
 
     @state_or_client.setter
     def state_or_client(self, new_s) -> None:
-        self.state_or_client_ = new_s
+        PropertiesCrawJUD.state_or_client_ = new_s
 
     @property
     def type_log(self) -> str:
-        return self.type_log_
+        return PropertiesCrawJUD.type_log_
 
     @type_log.setter
     def type_log(self, new_log) -> None:
-        self.type_log_ = new_log
+        PropertiesCrawJUD.type_log_ = new_log
 
     @property
     def pid(self) -> str:
-        return self.pid_
+        return PropertiesCrawJUD.pid_
 
     @pid.setter
     def pid(self, pid_) -> None:
-        self.pid_ = pid_
+        PropertiesCrawJUD.pid_ = pid_
 
     @property
     def message(self) -> str:
-        return self.message_
+        return PropertiesCrawJUD.message_
 
     @message.setter
     def message(self, new_msg) -> None:
-        self.message_bot = new_msg
+        PropertiesCrawJUD.message_bot = new_msg
 
     @property
     def driver(self) -> WebDriver:
-        return self.driver_
+        return PropertiesCrawJUD.driver_
 
     @driver.setter
     def driver(self, new_driver_: WebDriver) -> None:
-        self.driver_ = new_driver_
+        PropertiesCrawJUD.driver_ = new_driver_
 
     @property
     def wait(self) -> WebDriverWait:
-        return self.webdriverwait_
+        return PropertiesCrawJUD.webdriverwait_
 
     @wait.setter
     def wait(self, new_webdriverwait_: WebDriverWait) -> None:
-        self.webdriverwait_ = new_webdriverwait_
+        PropertiesCrawJUD.webdriverwait_ = new_webdriverwait_
 
     @property
     def chr_dir(self) -> Path:
-        return self.user_data_dir
+        return PropertiesCrawJUD.user_data_dir
 
     @chr_dir.setter
     def chr_dir(self, new_path: Path) -> None:
@@ -163,11 +163,11 @@ class PropertiesCrawJUD:
         if not isinstance(new_path, Path):
             raise ValueError("Path must be a Path object")
 
-        self.user_data_dir = new_path
+        PropertiesCrawJUD.user_data_dir = new_path
 
     @property
     def output_dir_path(self) -> Path:
-        return self.out_dir
+        return PropertiesCrawJUD.out_dir
 
     @output_dir_path.setter
     def output_dir_path(self, new_path: Path) -> None:
@@ -175,59 +175,59 @@ class PropertiesCrawJUD:
         if not isinstance(new_path, Path):
             raise ValueError("Path must be a Path object")
 
-        self.out_dir = new_path
+        PropertiesCrawJUD.out_dir = new_path
 
     @property
     def kwrgs(self) -> Dict[str, TypeValues | SubDict]:
-        return self.kwrgs_
+        return PropertiesCrawJUD.kwrgs_
 
     @kwrgs.setter
     def kwrgs(self, new_kwg: Dict[str, Any]) -> None:
-        self.kwrgs_ = new_kwg
+        PropertiesCrawJUD.kwrgs_ = new_kwg
 
     @property
     def row(self) -> Type[int]:
-        return self.row_
+        return PropertiesCrawJUD.row_
 
     @row.setter
     def row(self, new_row: int) -> None:
-        self.row_ = new_row
+        PropertiesCrawJUD.row_ = new_row
 
     @property
     def message_error(self) -> str:
-        return self.message_error_
+        return PropertiesCrawJUD.message_error_
 
     @message_error.setter
     def message_error(self, nw_m) -> str:
-        self.message_error_ = nw_m
+        PropertiesCrawJUD.message_error_ = nw_m
 
     @property
     def graphicMode(self) -> str:
-        return self.graphicMode_
+        return PropertiesCrawJUD.graphicMode_
 
     @graphicMode.setter
     def graphicMode(self, new_graph) -> None:
-        self.graphicMode_ = new_graph
+        PropertiesCrawJUD.graphicMode_ = new_graph
 
     @property
     def bot_data(self) -> dict[str, str | Numbers]:
-        return self.bot_data_
+        return PropertiesCrawJUD.bot_data_
 
     @bot_data.setter
     def bot_data(self, new_botdata: dict[str, str | Numbers]) -> None:
-        self.bot_data_ = new_botdata
+        PropertiesCrawJUD.bot_data_ = new_botdata
 
     @property
     def vara(self) -> str:
-        return self.vara_
+        return PropertiesCrawJUD.vara_
 
     @vara.setter
     def vara(self, vara_str) -> None:
-        self.vara_ = vara_str
+        PropertiesCrawJUD.vara_ = vara_str
 
     @property
     def path_accepted(self) -> Path:
-        return self.path_accepted_
+        return PropertiesCrawJUD.path_accepted_
 
     @path_accepted.setter
     def path_accepted(self, new_path) -> None:
@@ -235,32 +235,32 @@ class PropertiesCrawJUD:
         if not isinstance(new_path, Path):
             raise ValueError("Path must be a Path object")
 
-        self.path_accepted_ = new_path
+        PropertiesCrawJUD.path_accepted_ = new_path
 
     @property
     def OpenAI_client(self) -> OpenAI:
-        return self.OpenAI_
+        return PropertiesCrawJUD.OpenAI_
 
     @property
     def typebot(self) -> str:
-        return self.type_bot
+        return PropertiesCrawJUD.type_bot
 
     @typebot.setter
     def typebot(self, type_bot) -> None:
-        self.type_bot = type_bot
+        PropertiesCrawJUD.type_bot = type_bot
 
     @property
     def state(self) -> str:
-        return self.state_
+        return PropertiesCrawJUD.state_
 
     @state.setter
     def state(self, state_: str) -> None:
-        self.state_ = state_
+        PropertiesCrawJUD.state_ = state_
 
     @property
     def path_erro(self) -> Path:
 
-        return self.path_erro_
+        return PropertiesCrawJUD.path_erro_
 
     @path_erro.setter
     def path_erro(self, new_path: Path) -> None:
@@ -268,23 +268,23 @@ class PropertiesCrawJUD:
         if not isinstance(new_path, Path):
             raise ValueError("Path must be a Path object")
 
-        self.path_erro_ = new_path
+        PropertiesCrawJUD.path_erro_ = new_path
 
     @property
     def name_cert(self) -> str:
-        return self.name_cert_
+        return PropertiesCrawJUD.name_cert_
 
     @name_cert.setter
     def name_cert(self, name_cert) -> None:
-        self.name_cert_ = name_cert
+        PropertiesCrawJUD.name_cert_ = name_cert
 
     @property
     def client(self) -> str:
-        return self.client_
+        return PropertiesCrawJUD.client_
 
     @client.setter
     def client(self, client_) -> None:
-        self.client_ = client_
+        PropertiesCrawJUD.client_ = client_
 
     @property
     def AuthBot(self) -> Callable[[], bool]:
