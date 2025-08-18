c2e4a7af805da18d29f0b383984d37492c686f81
Robotz213 <nicholas@robotz.dev>
2025-08-18 19:12:21 -0400

    🔧 *refactor: remover imports não utilizados e limpar código*
    Removidos imports não utilizados para melhorar a legibilidade e a manutenção do código.
    
    Arquivos Alterados:
    - *crawjud/bots/csi/chamados.py*: remoção de imports desnecessários e limpeza do código.

--- crawjud/bots/csi/chamados.py
+++ crawjud/bots/csi/chamados.py
@@ -8,33 +8,18 @@ tratamento de contexto e execução assíncrona de tarefas.
 
 from __future__ import annotations
 
-import secrets  # noqa: F401
-import traceback  # noqa: F401
-from contextlib import suppress  # noqa: F401
-from threading import Thread  # noqa: F401
 from time import sleep
-from typing import TYPE_CHECKING, ClassVar  # noqa: F401
 
 from dotenv import load_dotenv
-from httpx import Client  # noqa: F401
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from tqdm import tqdm
 
-from crawjud.common.exceptions.bot import ExecutionError  # noqa: F401
 from crawjud.controllers.csi import CsiBot
-from crawjud.controllers.pje import PjeBot  # noqa: F401
 from crawjud.custom.task import ContextTask
 from crawjud.decorators import shared_task, wrap_cls
 from crawjud.resources.elements import csi as el
-from crawjud.utils.formatadores import formata_tempo  # noqa: F401
 
-if TYPE_CHECKING:
-    from concurrent.futures import Future  # noqa: F401
-    from datetime import datetime  # noqa: F401
-
-    from crawjud.interfaces.types import BotData  # noqa: F401
-    from crawjud.interfaces.types.pje import DictResults  # noqa: F401
 load_dotenv()
 
 
