db29675e8354cb6a31727b86b56b237b960aedf6
Robotz213 <nicholas@robotz.dev>
2025-09-04 18:02:03 -0400

    ✨ *add(bots): adicionar tipos para os bots CrawJUD*
    Adiciona novos tipos para representar a estrutura de dados dos bots, facilitando a tipagem e documentação.
    
    Files Changed:
    - *crawjud/interfaces/types/bots/__init__.py*: cria o arquivo com a documentação dos tipos dos bots.
    - *crawjud/interfaces/types/bots/pje.py*: implementa tipos para a estrutura de dados do PJe, incluindo dicionários tipados.
    - *crawjud/interfaces/types/bots/projudi.py*: cria tipos para os robôs do PROJUDI, mapeando o retorno de sucesso do protocolo.

---
+++ crawjud/interfaces/types/bots/__init__.py
@@ -0,0 +1,1 @@
+"""Types dos bots CrawJUD."""
---
+++ crawjud/interfaces/types/bots/projudi.py
@@ -0,0 +1,11 @@
+"""Tipos para robôs do PROJUDI."""
+
+from typing import TypedDict
+
+
+class DataSucessoProtocoloProjudi(TypedDict):
+    """Class dict para mapear retorno de sucesso Projudi Protocolo."""
+
+    NUMERO_PROCESSO: str
+    MENSAGEM: str
+    NOME_COMPROVANTE: str
