0720e97deb6ac0f739cbd6f3f32c6a9b470d0ed5
Robotz213 <nicholas@robotz.dev>
2025-10-13 12:56:48 -0300

    🔧 *refatorar: melhorar a inicialização do banco de dados*
    Ajusta a função de inicialização do banco de dados para garantir que o usuário root e a licença sejam criados corretamente, caso não existam. Remove a importação desnecessária do dotenv.
    
    Files Changed:
    - *app/models/__init__.py*: refatora a função init_database para incluir verificação e adição do usuário root e sua licença.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from typing import TypedDict
 
-from dotenv import dotenv_values
 from flask import Flask
 
 from app.models.users import LicenseUser, User
@@ -14,8 +13,6 @@ __all__ = [
     "LicenseUser",
     "User",
 ]
-
-environ = dotenv_values()
 
 
 class DatabaseInitEnvDict(TypedDict):
@@ -28,5 +25,34 @@ class DatabaseInitEnvDict(TypedDict):
 
 def init_database(app: Flask) -> None:
     """Inicializa o banco de dados."""
-    with app.app_context():
+    with app.app_context(), db.session.no_autoflush:
         db.create_all()
+        cfg = app.config
+
+        usr_check = db.session.query(User)
+        usr_check = usr_check.filter_by(login=cfg["ROOT_USERNAME"])
+        usr_check = usr_check.first()
+
+        if not usr_check:
+            to_add = []
+            root_user = User(
+                login=cfg["ROOT_USERNAME"],
+                password=cfg["ROOT_PASSWORD"],
+                email=cfg["ROOT_EMAIL"],
+                client=cfg["ROOT_CLIENT"],
+                cpf_cnpj_client=cfg["ROOT_CPF_CNPJ_CLIENT"],
+            )
+            lic = db.session.query(LicenseUser)
+            lic = lic.filter_by(desc="Root License")
+            lic = lic.first()
+
+            to_add.append(root_user)
+
+            if lic:
+                root_license = LicenseUser(desc="Root License")
+                root_user.license_id = root_license.Id
+
+                to_add.append(root_license)
+
+            db.session.add_all(to_add)
+            db.session.commit()
