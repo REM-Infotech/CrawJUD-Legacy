6020d4ed59d14d553221c873c38d326a82385a24
Robotz213 <nicholas@robotz.dev>
2025-10-14 11:53:10 -0300

    Refactor App.vue and remove unused assets
    - Cleaned up App.vue by removing HelloWorld component and related styles.
    - Deleted base.css and replaced it with main.css for Tailwind CSS integration.
    - Added TypeScript definitions for main.css.
    - Removed unused logo.svg asset.
    - Created a new empty file in assets for future use.
    - Updated HelloWorld.vue to fix TypeScript prop definition.
    - Standardized import statements in TheWelcome.vue.
    - Fixed whitespace content in WelcomeItem.vue.
    - Updated HelloWorld.spec.ts to use consistent import styles.
    - Refactored main.ts to include PrimeVue and Aura theme.
    - Simplified router configuration by removing unused routes.
    - Updated counter store to use consistent import styles.
    - Deleted AboutView.vue and HomeView.vue as they are no longer needed.
    - Created new AuthView.vue and FormView.vue for authentication.
    - Added new HomeView.vue with basic template.
    - Updated vite.config.ts to include Tailwind CSS plugin.

--- app-ui/src/main.ts
+++ app-ui/src/main.ts
@@ -1,14 +1,24 @@
-import './assets/main.css'
+import "@/assets/css/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia } from "pinia";
+import { createApp } from "vue";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import Aura from "@primeuix/themes/aura";
+import PrimeVue from "primevue/config";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+const pinia = createPinia();
 
-app.mount('#app')
+app.use(pinia);
+app.use(router);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
+app.mount("#app");
