4cc61002e3e9fd644d7414d9d96c83f23c9a4190
Robotz213 <nicholas@robotz.dev>
2025-10-14 10:09:42 -0300

    🔧 *refatorar: ajustar configurações de socket e dependências*
    Ajustes nas configurações do servidor e dependências para melhorar a performance e compatibilidade.
    
    Files Changed:
    - *.vscode/launch.json*: corrigida a configuração do módulo para incluir `justMyCode`.
    - app/__init__.py: alterado o modo assíncrono do SocketIO de "threading" para "eventlet".
    - app/__main__.py: removido o uso do `io.run` e ajustada a inicialização do servidor.
    - pyproject.toml: adicionada a dependência `gevent-websocket`.
    - uv.lock: atualizado para incluir a nova dependência `gevent-websocket`.

--- app/__main__.py
+++ app/__main__.py
@@ -2,20 +2,15 @@
 
 from aether import ThreadPoolWSGIServer
 
-from app import create_app, io
+from app import create_app
 
 app = create_app()
 
-
-server_thread = ThreadPoolWSGIServer(
+with ThreadPoolWSGIServer(
     host="0.0.0.0",  # noqa: S104
     port=5002,
     app=app,
     poll_interval=0.25,
     max_workers=64,
-)
-
-io.run(app, port=5001, host="0.0.0.0")  # noqa: S104
-
-with server_thread as th:
+) as th:
     th.run()
