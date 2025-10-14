91e286947b368cc7b62686b1caca63a26d168788
Robotz213 <nicholas@robotz.dev>
2025-10-14 15:59:37 -0300

    refactor: clean up CSS and SCSS formatting, improve consistency in input type definitions, and streamline JSON configuration

--- app-ui/src/router/index.ts
+++ app-ui/src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "login",
-      component: () => import("@/views/LoginView.vue"),
+      component: () => import("@/views/auth/LoginView.vue"),
     },
     {
       path: "/dashboard",
@@ -32,35 +32,11 @@ const router = createRouter({
         name: "dashboard",
       },
     },
-    {
-      path: "/credentials",
-      name: "credentials",
-      component: () => import("@/views/credentials/CredentialsView.vue"),
-    },
-    {
-      path: "/credentials/new",
-      name: "newCredential",
-      component: () => import("@/views/credentials/form/CredentialsFormView.vue"),
-    },
-    {
-      path: "/configuracoes",
-      name: "configuracoes",
-      component: () => import("@/views/config/ConfiguracoesView.vue"),
-    },
-    {
-      path: "/usuario/:metodo",
-      name: "newUser",
-      component: () => import("@/views/config/form/UserConfigFormView.vue"),
-    },
+
     {
       path: "/bots",
       name: "bots",
       component: () => import("@/views/bots/BotsView.vue"),
-    },
-    {
-      path: "/bot/:bot_id/:bot_system/:bot_type",
-      name: "bot_form",
-      component: () => import("@/views/botform/BotForm.vue"),
     },
   ],
 });
--- app-ui/src/views/botform/setup/index.d.ts
+++
@@ -1,3 +0,0 @@
-interface Beta {
-  tuebetacara: string;
-}
