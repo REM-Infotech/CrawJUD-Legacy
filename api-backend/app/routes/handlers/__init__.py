836677834e35bc5e33f76edf41285c442aa91fc7
Robotz213 <nicholas@robotz.dev>
2025-10-20 16:26:52 -0300

    🔧 *refact: remover código obsoleto do log do bot*
    Removido código não utilizado relacionado ao log do bot e organizado as importações.
    
    Files Changed:
    - *api-backend/app/resources/extensions.py*: removidos métodos e importações não utilizados relacionados ao log do bot.
    - *api-backend/app/routes/handlers/__init__.py*: adicionado código para gerenciar eventos de log do bot.

--- api-backend/app/routes/handlers/__init__.py
+++ api-backend/app/routes/handlers/__init__.py
@@ -1,1 +1,26 @@
 """Log bot."""
+
+from __types import MyAny
+from _interfaces import Message
+from flask_socketio import join_room
+from tqdm import tqdm
+
+from app.resources.extensions import io
+
+
+@io.on("connect", namespace="/bot_logs")
+def connected(*args: MyAny, **kwargs: MyAny) -> None:
+    """Log bot."""
+
+
+@io.on("join_room", namespace="/bot_logs")
+def join_room_bot(data: dict[str, str]) -> None:
+    """Log bot."""
+    join_room(data["room"])
+
+
+@io.on("logbot", namespace="/bot_logs")
+def log_bot(data: Message) -> None:
+    """Log bot."""
+    tqdm.write(data["message"])
+    io.emit("logbot", data=data, room=data["pid"], namespace="/bot_logs")
