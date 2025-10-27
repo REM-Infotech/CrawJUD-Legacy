3a8d10a77c2e56d710c8797ce11477500096c92c
Robotz213 <nicholas@robotz.dev>
2025-10-27 13:20:56 -0300

    ✨ *feat: adicionar suporte ao Socket.io*
    Adiciona a implementação do Socket.io para comunicação em tempo real na aplicação.
    
    Files Changed:
    - *app-ui/app/plugins/socketio.ts*: novo arquivo que configura o cliente Socket.io com credenciais e cabeçalhos extras.
    - *app-ui/package.json*: adiciona a dependência `socket.io-client` para suporte ao Socket.io.

---
+++ app-ui/app/plugins/socketio.ts
@@ -0,0 +1,17 @@
+import { Manager } from "socket.io-client";
+export default defineNuxtPlugin((_) => {
+  const uri = new URL("", import.meta.env.VITE_API_URL).toString();
+  const socketio = new Manager(uri, {
+    withCredentials: true,
+    autoConnect: true,
+    extraHeaders: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return {
+    provide: {
+      socketio,
+    },
+  };
+});
