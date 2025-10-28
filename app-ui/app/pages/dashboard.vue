31b526e7500871e9c577eb5699905cc186024094
Robotz213 <nicholas@robotz.dev>
2025-10-28 13:48:22 -0300

    🔧 *refactor: remover objeto bot não utilizado*
    Removido o objeto `bot` que não estava sendo utilizado no componente `dashboard.vue`, simplificando o código e melhorando a legibilidade.
    
    Files Changed:
    - *app-ui/app/pages/dashboard.vue*: remoção do objeto `bot` não utilizado.

--- app-ui/app/pages/dashboard.vue
+++ app-ui/app/pages/dashboard.vue
@@ -2,17 +2,6 @@
 import DataTable from "datatables.net-vue3";
 import AppCard from "~/components/AppCard.vue";
 
-const bot = {
-  id: "21",
-  display_name: "JUSDS - Realizar Compromisso",
-  system: "JUSDS",
-  state: "EVERYONE",
-  client: "INFRAERO",
-  type: "REALIZA_PRAZO",
-  form_cfg: "multipe_files",
-  classification: "ADMINISTRATIVO",
-  text: "Robô responsável por realizar compromissos no JUSDS",
-};
 const columns = [
   { data: "id", title: "#" },
   { data: "display_name", title: "Nome" },
@@ -21,7 +10,6 @@ const columns = [
 ];
 
 const { $api } = useNuxtApp();
-const botsResp = ref();
 
 async function get_bot(_: unknown, callback: Function, __: unknown) {
   const resp = await $api.get("/bots/listagem");
