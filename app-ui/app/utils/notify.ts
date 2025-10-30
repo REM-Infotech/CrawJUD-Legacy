f7cc36888b1428c76ab1112891ffb42ed6f10865
Robotz213 <nicholas@robotz.dev>
2025-10-30 14:36:14 -0300

    🔧 *refatorar: remover log desnecessário e ajustar ícone da notificação*
    Removido o console.log que exibia a imagem do ícone. O ícone da notificação agora é definido corretamente no método show.
    
    Files Changed:
    - *app-ui/app/utils/notify.ts*: removido log desnecessário e ajustado o ícone da notificação.

--- app-ui/app/utils/notify.ts
+++ app-ui/app/utils/notify.ts
@@ -1,11 +1,11 @@
 import crawjud2 from "~/assets/img/crawjud2.png";
 
-console.log(crawjud2);
-
 class CrawJUDNotification {
   constructor() {}
 
   show(args = { title: "CrawJUD" } as KeywordArgs) {
+    args.options["icon"] = crawjud2;
+
     new Notification(args.title || "CrawJUD", args.options);
   }
 }
